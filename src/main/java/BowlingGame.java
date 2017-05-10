d489507b41376ba844234a574e0557d91e5e8af8
Zero9988666 <448250123@qq.com>
2017-05-10 13:57:28 +0800

    Update BowlingGame.java

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,61 @@
 public class BowlingGame {
 
     public int getBowlingScore(String bowlingCode) {
-        return 0;
+    	if(bowlingCode.endsWith("|"))
+    		bowlingCode=bowlingCode+"-";
+    	String[] sss = bowlingCode.split("\\|");
+    	int res=0;
+    	sss[sss.length-2]=sss[sss.length-1];
+    	char[][] ssscc=new char[sss.length][3];
+    	int k=0;
+    	for(String s1:sss)
+    		ssscc[k++]=s1.toCharArray();
+    	for(int i=0;i<sss.length-2;i++){
+
+			if(sss[i].equals("X")){
+				if(sss[i+1].equals("X")){
+					if(sss[i+2].equals("X")){
+						res=res+30;
+					}else{
+						if(ssscc[i+2][0]=='X')
+							res=res+30;
+						else if(ssscc[i+2][0]=='-')
+							res=res+20;
+						else
+							res=res+20+ssscc[i+2][0]-'0';
+					}
+				}else if(sss[i+1].equals("XX")){
+					res=res+30;
+				}else{
+					if(ssscc[i+1][1]=='/'){
+						res=res+20;
+					}else{
+						res=res+10;
+						if(ssscc[i+1][0]!='-')
+							res=res+ssscc[i+1][0]-'0';
+						if(ssscc[i+1][1]!='-')
+							res=res+ssscc[i+1][1]-'0';
+					}
+				}
+			}else{
+				if(ssscc[i][1]=='/'){
+					res=res+10;
+					if(sss[i+1].equals("X"))
+						res=res+10;
+					else{
+						if(ssscc[i+1][0]!='-')
+							res=res+ssscc[i+1][0]-'0';
+					}
+				}else{
+					if(ssscc[i][0]!='-')
+						res=res+ssscc[i][0]-'0';
+					if(ssscc[i][1]!='-')
+						res=res+ssscc[i][1]-'0';
+				}
+			}
+
+    	}
+    	
+        return res;
     }
 }
